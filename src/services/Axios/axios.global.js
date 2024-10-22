9e6a98f330cb9b2f393ba5ffb7e52c77a8bc8177
AHMED FARES <ahmedfaresss416@gmail.com>
2024-10-22 08:31:33 +0300

    update api

--- src/services/Axios/axios.global.js
+++ src/services/Axios/axios.global.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api-for-e-commerce-wep-application.vercel.app";
+// axios.defaults.baseURL = "https://api-for-e-commerce-wep-application.vercel.app";
+axios.defaults.baseURL = "https://api-for-e-commerce.vercel.app/";
